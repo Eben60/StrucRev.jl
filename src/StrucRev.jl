ba9c4e0982c6f30e3d43fa6a805716162bbb64d3
Ben Elkin <8xe8n-cm@online.de>
2024-02-08 22:24:27 +0100

    initial commit

---
+++ src/StrucRev.jl
@@ -0,0 +1,107 @@
+module StrucRev
+using MacroTools
+using MacroTools: prewalk
+
+const packagename = string(@__MODULE__)
+
+# https://discourse.julialang.org/t/get-all-names-accessible-from-a-module/98492/10
+extract_variables!(s::Symbol; _names, _mod) = isdefined(_mod, s) && 
+    !(!(parentmodule(_mod) == Main) && isdefined(Main, s)) && 
+    !isdefined(Base, s) && 
+    push!(_names, s)
+
+extract_variables!(x; kwargs...) = nothing
+
+function extract_variables!(ex::Expr; kwargs...)
+    if ex.head === :.
+        extract_variables!(ex.args |> first; kwargs...)
+    else
+        foreach(x -> extract_variables!(x; kwargs...), ex.args)
+    end
+    return nothing
+end
+
+function extract_variables(ex, m)
+    _names = Set{Symbol}()
+    extract_variables!(ex; _names, _mod=m)
+    return sort(collect(_names))
+end
+
+function make_usings(ex, fname, m)
+    vars = extract_variables(ex, m)
+    setdiff!(vars, [fname])
+    varsstr = join(vars, ", ")    
+    exstr = isempty(vars) ? "" : "using $m: $varsstr"
+    return Meta.parse(exstr)
+end
+
+getname(x::Symbol) = x
+getname(ex::Expr) = getname(ex.args[1])
+
+getstructnames!(x, _names) = nothing
+
+function getstructnames!(ex::Expr, _names)
+    if ex.head == :struct
+        sname = getname(ex.args[2]) 
+        push!(_names, sname)
+    elseif ex.head in (:abstract, :primitive)
+        sname = getname(ex.args[1]) 
+        push!(_names, sname)       
+    else
+        foreach(x -> getstructnames!(x, _names), ex.args)
+    end
+    return nothing
+end
+
+function getstructname(arg)
+    _names = Symbol[]
+    getstructnames!(arg, _names)
+    isempty(_names) && error("Parsing error with @strev - no struct definitions found")
+    length(_names) > 1 && error("Parsing error with @strev - more than one struct definitions found")
+    sname = _names[1]
+    return sname
+end
+
+function strev_struct(arg, mod)
+    fname = getstructname(arg)
+    exusings = make_usings(arg, fname, mod)
+    blck = Expr(:block, exusings, arg)
+    modname = Symbol("$(packagename)_$(fname)")
+    wrappermod = Expr(:module, true, modname, blck) 
+    modname = wrappermod.args[2]
+    ex2 = :($fname = $modname.$fname)
+    ex3 = Expr(:toplevel, wrappermod, ex2)
+    return esc(ex3) 
+end
+
+function strev_const(arg)
+    arg = prewalk(rmlines, arg)
+    head = arg.head
+    args = arg.args
+    head == :const || error("Parsing error with @strev")
+    ex = args[1]
+    ex.head == :(=) || error("Parsing error with @strev")
+    # https://giordano.github.io/blog/2022-06-18-first-macro/
+    q = quote
+        local tmp = $(esc(ex.args[2]))
+        $(esc(ex.args[1]))::typeof(tmp) = tmp
+    end
+
+    return q
+end
+
+macro strev(arg)
+    mod = __module__
+    arg = rmlines(arg)
+    if arg.head in (:struct, :block, :abstract, :primitive)
+        return strev_struct(arg, mod)
+    elseif arg.head == :const 
+        return strev_const(arg)       
+    else
+        error("Parsing error with @strev")
+    end
+end
+
+export @strev
+
+end # module ModularWF
---
+++ test/runtests.jl
@@ -0,0 +1,145 @@
+module MainTests
+
+using StrucRev
+using Test
+
+function istypedglobal(m, v)
+    isdefined(m, v) || return false
+    getproperty(m, v) isa Function && return false
+    return Core.get_binding_type(m, v) != Any
+end
+
+struct F0
+    x::Int
+end
+
+@strev struct F3
+    x::F0
+end
+  
+@strev mutable struct F4
+    y
+end
+
+@strev begin 
+    struct F5
+        xx::F0
+    end
+    F5(x::Number) = F5(F0(x))
+    F5(x::String) = F5(F0(parse(Int, x)))
+end
+
+@strev struct F6
+    x1
+    x2::F0
+    F6(x1, x2::Int) = new(x1, F0(x2))
+end
+
+@strev abstract type A1 <: Number end
+@strev abstract type A2 <: A1 end
+
+@strev primitive type I08 <: A2 8 end
+@strev primitive type I16 16 end
+
+@strev struct F7 <: A2
+    x
+end
+
+@strev begin
+    struct F8{T, S} <: A1 where T <: Real where S <: AbstractString
+        r::T
+        s::S
+    end
+    F8(x::Real) = F8(x, repr(x))
+end
+
+@strev struct F9{T, S} <: A1 where T <: Real where S <: AbstractString
+    r::T
+    s::S
+end
+
+@strev mutable struct F1
+    a::Int
+    const b::Float64
+end
+
+@strev const x9 = 9 
+@strev const x10 = 2*5 
+@strev const x11 = 10 
+
+x11 = 11
+
+err11 = nothing
+try
+    global x11 = "12"
+    global err11 = false
+catch
+    global err11 = true
+end
+
+@testset "StrucRev" begin
+
+@test F1 === StrucRev_F1.F1
+@test F3 === StrucRev_F3.F3
+@test F4 === StrucRev_F4.F4
+@test F5 === StrucRev_F5.F5
+@test I08 === StrucRev_I08.I08
+@test I16 === StrucRev_I16.I16
+@test A1 === StrucRev_A1.A1
+@test A2 === StrucRev_A2.A2
+
+s1 = F1(1, 2.0)
+@test s1.a == 1
+@test s1.b == 2.0
+@test_throws ErrorException s1.b = 3.0
+
+s3 = F3(F0(3))
+@test s3.x.x == 3
+s4 = F4(4)
+@test s4.y == 4
+s4.y = 44
+@test s4.y == 44
+
+s5 = F5(F0(5))
+@test s5.xx.x == 5
+
+s5a = F5(5)
+s5b = F5("5")
+@test s5 == s5a == s5b
+
+s6 = F6(6, 7)
+@test s6.x1 == 6
+@test s6.x2.x == 7
+
+s7 = F7(7.0)
+@test s7.x == 7.0
+@test s7 isa Number
+@test s7 isa A1
+@test s7 isa A2
+@test A2 <: A1
+@test A2 <: Number
+
+@test I08 <: A2
+
+s8 = F8(4, "4")
+s8a = F8(4)
+@test s8 == s8a
+
+s9 = F9(3, "3")
+@test s9.r == 3
+@test s9.s == "3"
+
+@test x9 == 9
+@test x10 == 10
+@test x11 == 11
+
+@test istypedglobal(@__MODULE__, :x9)
+@test istypedglobal(@__MODULE__, :x10)
+
+# @test_throws MethodError 
+@test isdefined(@__MODULE__, :x9)
+@test err11
+
+end # testset
+
+end # module
